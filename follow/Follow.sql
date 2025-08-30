2903cb1b8dc8d662860edb3b42f70a9963bd9502
405supply <405supplychoi@gmail.com>
2025-08-30 19:42:32 +0900

    기능 구현 방식 변경 - 함수와 테스트코드 항목으로 구현함

--- follow/Follow.sql
+++ follow/Follow.sql
@@ -1,18 +1,52 @@
 -- 팔로우 기능 구현
 
 -- 팔로우하기
-INSERT INTO follow (following_id, followed_id)
-VALUES ('나의 id', '팔로우 대상의 id');
+DELIMITER //
+
+CREATE FUNCTION add_follow(following INT, followed INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	INSERT INTO follow (following_id, followed_id) VALUES (following, followed);
+END //
+
+DELIMITER ;     
+
+-- 테스트 코드
+add_follow(2, 5);
 
 
 -- 팔로우 취소하기
-UPDATE follow SET is_deleted = TRUE
-WHERE following_id = '나의 id', '팔로우 대상의 id');
+DELIMITER //
+
+CREATE FUNCTION cancle_follow(following INT, followed INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	UPDATE follow SET is_deleted = TRUE
+	WHERE following_id = following AND followed_id = followed;
+END //
+
+DELIMITER ;
+
+-- 테스트 코드
+cancle_follow(2, 5);
 
 
--- 내가 팔로우 하고 있는 유저들의 id 및 팔로우 시간 조회 
-SELECT followed_id, created_at FROM follow
-WHERE following_id = '찾고자하는아이디' AND is_deleted = FALSE;
+-- 내가 팔로우 하고 있는 유저들의 id 및 팔로우 시간 조회
+DELIMITER //
+
+CREATE FUNCTION select_my_followed(user_id INT)
+RETURN INT
+DETERMINISTIC
+BEGIN
+	SELECT followed_id, created_at FROM follow
+	WHERE following_id = user_id AND is_deleted = FALSE;
+END //
+
+DELIMITER ;
+
+select_my_followed(7);
 
 
 -- 나를 팔로우 하고 있는 유저들의 id 및 팔로우 시간 조회
