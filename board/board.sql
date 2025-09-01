12a7de71a811c76b085d1143d5b5972839cab2c8
405supply <405supplychoi@gmail.com>
2025-09-01 16:54:58 +0900

    board 구현 내용 리펙토링 및 테스트 코드 추가

--- board/board.sql
+++ board/board.sql
@@ -1,91 +1,154 @@
 -- 게시판 기능 구현
 
 -- 전체 게시글 목록 조회
-SELECT board_id
-	  , category_id
-	  , user_id
-	  , title
-	  , created_at
-  FROM board
- WHERE is_deleted = 0
-ORDER BY = created_at DESC;
+DROP PROCEDURE if exists select_all_board;
+DELIMITER //
+
+CREATE PROCEDURE select_all_board()
+BEGIN
+	SELECT board_id
+		  , category_id
+		  , user_id
+		  , title
+		  , created_at
+	  FROM board
+	 WHERE is_deleted = 0
+	ORDER BY created_at DESC;
+END //
+DELIMITER ;
 
 -- 카테고리 별 게시글 목록 조회
-SELECT board_id
-	  , user_id
-	  , title
-	  , created_at
-  FROM board
- WHERE category_id = 4
- 	AND is_deleted = 0
-ORDER BY created_at DESC;
+DROP PROCEDURE if exists select_board_by_category;
+DELIMITER //
+
+CREATE PROCEDURE select_board_by_category(IN _category_id INT)
+BEGIN
+	SELECT board_id
+		  , user_id
+		  , title
+		  , created_at
+	  FROM board
+	 WHERE category_id = _category_id
+	 	AND is_deleted = 0
+	ORDER BY created_at DESC;
+END //
+DELIMITER ;
+
 
 -- 게시글 상세 조회
-SELECT board_id
-	  , category_id
-	  , user_id
-	  , title
-	  , text
-	  , created_at
-	  , updated_at
-  FROM board
- WHERE board_id = 1
- 	AND is_deleted = 0;
+DROP PROCEDURE if exists select_board_specific;
+DELIMITER //
+
+CREATE PROCEDURE select_board_specific(IN _board_id INT)
+BEGIN
+	SELECT board_id
+		  , category_id
+		  , user_id
+		  , title
+		  , text
+		  , created_at
+		  , updated_at
+	  FROM board
+	 WHERE board_id = _board_id
+	 	AND is_deleted = 0;
+END //
+DELIMITER ;
 	  
 -- 게시글 신고
-INSERT INTO board_report (user_id, board_id, reason, created_at, is_handled)
-VALUES (17, 5, NULL, NOW(), 0);
+DROP PROCEDURE if exists report_board;
+DELIMITER //
+CREATE PROCEDURE report_board(IN _user_id INT, IN _board_id INT, IN _reason TEXT)
+BEGIN
+	INSERT INTO board_report (user_id, board_id, reason, created_at, is_handled)
+	VALUES (_user_id, _board_id, _reason, NOW(), 0);
+END //
+DELIMITER ;	
 
--- 게시글 등록
-INSERT INTO board(category_id, user_id, title, TEXT)
-VALUES (1, 23, '게시글 제목입니다', '제목이 곧 내용');
+	-- 게시글 등록
+	DROP PROCEDURE if exists insert_board;
+DELIMITER //
+CREATE PROCEDURE insert_board(
+	IN _category_id INT,
+	IN _user_id INT, 
+	IN _title VARCHAR(80),
+	IN _text TEXT
+	)
+BEGIN
+	INSERT INTO board(category_id, user_id, title, TEXT)
+	VALUES (_category_id, _user_id, _title, _text);
+END //
+DELIMITER ;
 
 -- 게시글 수정
-UPDATE board
-		 SET title = '게시글 수정합니다'
-     , TEXT = '제목이 곧 내용'
-     , updated_at = CURRENT_TIMESTAMP
- WHERE board_id = 4
- 	AND user_id = 17
- 	AND is_deleted = 0;
+DROP PROCEDURE if exists edit_board;
+DELIMITER //
+CREATE PROCEDURE edit_board(
+	IN _title VARCHAR(80),
+	IN _text TEXT,
+	IN _board_id INT,
+	IN _user_id INT
+)
+BEGIN
+	UPDATE board
+			 SET title = _title
+	     , TEXT = _text
+	     , updated_at = CURRENT_TIMESTAMP
+	 WHERE board_id = _board_id
+	 	AND user_id = _user_id
+	 	AND is_deleted = 0;
+END //
+DELIMITER ;
+
 
 -- 게시글 삭제
-START TRANSACTION
-
-UPDATE board
-SET is_deleted = TRUE
-WHERE board_id = 5
-AND user_id = 17
-
-UPDATE user
-SET POINT  = POINT -10
-WHERE user_id = 17
-
-INSERT INTO point_log(user_id, TYPE, ACTION, fluctuation, created_at)
-VALUES (17, 'post', 'delete', -10, NOW());
-
+DROP PROCEDURE if exists delete_board;
+DELIMITER //
+CREATE PROCEDURE delete_board(
+	IN _board_id INT
+)
+BEGIN
+	UPDATE board
+	SET is_deleted = TRUE
+	WHERE board_id = _board_id;
+END //
+delimiter ;
+	
 -- 게시글 추천 비추천
-INSERT INTO board_recommend (user_id , board_id, TYPE)
-VALUES (17, 5, 'U');
-INSERT INTO board_recommend (user_id , board_id, TYPE)
-VALUES (17, 5, 'D');
+DROP PROCEDURE if exists recommend_board;
+DELIMITER //
+CREATE PROCEDURE recommend_board(
+	IN _user_id INT,
+	IN _board_id INT,
+	IN _type ENUM('U', 'D')
+)
+BEGIN
+	INSERT INTO board_recommend (user_id , board_id, TYPE)
+	VALUES (_user_id, _board_id, _type);
+END //
+DELIMITER ;
 
 -- hot 게시판
-SELECT b.board_id,
-       b.title,
-       b.user_id,
-       SUM(CASE WHEN br.type = 'U' THEN 1
-                WHEN br.type = 'D' THEN -1
-                ELSE 0 END) AS score,
-       COUNT(CASE WHEN br.type = 'U' THEN 1 END) AS up_count,
-       COUNT(CASE WHEN br.type = 'D' THEN 1 END) AS down_count,
-       b.created_at
-FROM board b
-LEFT JOIN board_recommend br ON b.board_id = br.board_id
-WHERE b.is_deleted = FALSE
-  AND b.created_at >= NOW() - INTERVAL 3 HOUR
-GROUP BY b.board_id, b.title, b.user_id, b.created_at
-HAVING score > 0
-ORDER BY score DESC, up_count DESC, b.created_at DESC
-LIMIT 10;
+DROP PROCEDURE if exists select_popular_board;
+DELIMITER //
+CREATE PROCEDURE select_popular_board()
+BEGIN
+	SELECT b.board_id,
+	       b.title,
+	       b.user_id,
+	       SUM(CASE WHEN br.type = 'U' THEN 1
+	                WHEN br.type = 'D' THEN -1
+	                ELSE 0 END) AS score,
+	       COUNT(CASE WHEN br.type = 'U' THEN 1 END) AS up_count,
+	       COUNT(CASE WHEN br.type = 'D' THEN 1 END) AS down_count,
+	       b.created_at
+	FROM board b
+	LEFT JOIN board_recommend br ON b.board_id = br.board_id
+	WHERE b.is_deleted = FALSE
+	  AND b.created_at >= NOW() - INTERVAL 3 HOUR
+	GROUP BY b.board_id, b.title, b.user_id, b.created_at
+	HAVING score > 0
+	ORDER BY score DESC, up_count DESC, b.created_at DESC
+	LIMIT 10;
+END //
+DELIMITER ;
 
---
+++ board/board_test.sql
@@ -0,0 +1,41 @@
+-- 테스트 코드
+-- ===========
+
+CALL select_all_board();
+
+CALL select_board_by_category(6);
+
+CALL select_board_specific(5);
+
+CALL report_board(17, 4, "광고글임");
+SELECT * from board_report ORDER BY created_at DESC LIMIT 3; 
+
+CALL insert_board(
+	2,
+	4, 
+	"반갑게시글제목",
+	"안녕게시글내용"
+	);
+SELECT * FROM board ORDER BY created_at DESC LIMIT 3;
+	
+CALL edit_board(
+	"수정한게시글제목",
+	"수정한게시글내용",
+	41,
+	4
+);
+CALL select_board_specific(41);
+
+
+CALL delete_board(41);
+CALL select_board_specific(41);
+
+CALL recommend_board(
+	6,
+	29,
+	'U'
+);
+SELECT * FROM board_recommend;
+
+
+CALL select_popular_board();
