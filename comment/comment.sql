2375b6d84ad0467892781a3d65de101b6181bdf3
405supply <405supplychoi@gmail.com>
2025-09-01 18:50:58 +0900

    댓글 구현 내용 리펙토링

--- comment/comment.sql
+++ comment/comment.sql
@@ -1,87 +1,64 @@
 -- 댓글 기능 구현
 
 -- 댓글 등록
-INSERT INTO comment (user_id, text)
-VALUES (1, '첫 번째 댓글 등록');
+DROP PROCEDURE if EXISTS insert_comment;
 
--- 대댓글 등록
-INSERT INTO comment (user_id, text, parent_comment_id)
-VALUES (2, '첫 번째 대댓글 등', 16);
-
--- 단어 필터링 기능
-
--- 댓글 포인트 기능
-START TRANSACTION;
-
-INSERT INTO comment (user_id, TEXT)
-VALUES (13, '포인트 냠냠')
-
-UPDATE user
-SET POINT = POINT + 5
-WHERE user_id = 13
-
-INSERT INTO point_log (user_id, TYPE, ACTION, fluctuation, created_at)
-VALUES (13, 'comment', 
+DELIMITER //
+CREATE PROCEDURE insert_comment
+(IN _user_id INT,
+ IN _board_id INT,
+ IN _text TEXT,
+ IN _parent_comment_id INT
+)
+BEGIN
+	INSERT INTO comment(user_id, board_id, text, parent_comment_id)
+	VALUES(_user_id, _board_id, _text, _parent_comment_id);
+END //
+DELIMITER ;
 
 -- 댓글 조회
-SELECT comment_ref
-     , a.comment_id
-	  , a.text
-	  , a.created_at
-	  , b.nickname
-	  , c.up_count
-	  , c.down_count
-  FROM (
-    SELECT CONCAT(comment_id,"_0") AS comment_ref
-    , oc.*
-    FROM comment oc
-    WHERE parent_comment_id IS NULL
-    UNION ALL
-    SELECT CONCAT(parent_comment_id,"_", comment_id) AS comment_ref
-    , rc.*
-    FROM comment rc
-    WHERE parent_comment_id IS NOT NULL
-       ) as a
-  JOIN user b ON a.user_id = b.user_id
-  JOIN (
-        SELECT comment_id,
-           SUM(CASE WHEN type = 'u' THEN 1 ELSE 0 END) AS up_count,
-           SUM(CASE WHEN type = 'd' THEN 1 ELSE 0 END) AS down_count
-        FROM comment_recommend
-        GROUP BY comment_id
-       ) c ON c.comment_id =  a.comment_id
--- WHERE a.comment_ref NOT LIKE '%_0'
-ORDER BY CAST(SUBSTRING_INDEX(a.comment_ref, '_', 1) AS UNSIGNED)
-,CAST(SUBSTRING_INDEX(a.comment_ref, '_', -1) AS UNSIGNED);player_comment
+DROP PROCEDURE if EXISTS select_comment;
+
+DELIMITER //
+CREATE PROCEDURE select_comment(IN _board_id INT)
+BEGIN
+	SELECT comment_id, parent_comment_id, user_id, TEXT, created_at, updated_at
+	FROM comment
+	WHERE board_id = _board_id;
+END //
+DELIMITER ;
 
 -- 댓글 수정
-UPDATE comment
-SET TEXT = '수정된 댓글 입니다.'
-UPDATE AT = NOW()
-WHERE comment_id = 2
-  AND user_id = 24;
+DROP PROCEDURE if EXISTS edit_comment;
 
--- 댓글 삭제 (포인트 회수)
-START TRANSACTION
-
-UPDATE comment
-SET is_deleted = TRUE
-WHERE commet_id = 5
-AND user_id = 17
-
-UPDATE user
-SET POINT  = POINT - 5
-WHERE user_id = 17
-
-INSERT INTO point_log(user_id, TYPE, ACTION, fluctuation, created_at)
-VALUES (17, 'comment', 'delete', -5, NOW());
+DELIMITER //
+CREATE PROCEDURE edit_comment(IN _comment_id INT, IN _text TEXT)
+BEGIN
+	UPDATE comment
+	SET TEXT = _text,
+	updated_at = NOW()
+	WHERE comment_id = _comment_id;
+END //
+DELIMITER ;
 
 -- 댓글 추천
-INSERT INTO comment_recommend (user_id , comment_id, TYPE)
-VALUES (17, 5, 'U')
-INSERT INTO comment_recommend (user_id , comment_id, TYPE)
-VALUES (17, 5, 'D')
+DROP PROCEDURE if EXISTS recommend_comment;
+
+DELIMITER //
+CREATE PROCEDURE recommend_comment(IN _user_id INT, IN _comment_id INT, IN _type ENUM('U', 'D'))
+BEGIN
+	INSERT INTO comment_recommend (user_id , comment_id, TYPE)
+	VALUES (_user_id, _comment_id, _type);
+END //
+DELIMITER ;
 
 -- 댓글 신고
-INSERT INTO comment_report (user_id, comment_id, reason, created_at, is_handled)
-VALUES (17, 5, NULL, NOW(), 0)
+DROP PROCEDURE if EXISTS report_comment;
+
+DELIMITER //
+CREATE PROCEDURE report_comment(IN _user_id INT, IN _comment_id INT, IN _reason TEXT)
+BEGIN
+	INSERT INTO comment_report (user_id, comment_id, reason)
+	VALUES (_user_id, _comment_id, _reason);
+END //
+DELIMITER ;
